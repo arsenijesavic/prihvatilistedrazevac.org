c9bffcd36596c0e5ce6b073adf488d1130b01ea6
Arsenije Savic <arsenijesavic@Arsenijes-MacBook-Pro.local>
2021-02-03 14:08:08 +0100

    first commit

---
+++ src/_data/site.js
@@ -0,0 +1,32 @@
+module.exports = {
+  buildTime: new Date(),
+  baseUrl: "https://drazevacshelter.org",
+  name: "drazevac-shelder",
+  twitter: "",
+  languages: [
+    {
+      label: "english",
+      code : "en"
+    },
+    {
+      label: "serbia",
+      code: "rs"
+    }
+  ],
+    en: {
+        metaTitle: "Multilingual Eleventy",
+        metaDescription: "A multilingual eleventy website demo",
+        back: "Back",
+        skipText: "Skip to the main content"
+        index: {
+            heroTitle: "Draževac Shelter"
+            heroSubtitle:"Temporary house for animals who are looking for a home"
+        }
+    },
+    rs: {
+        metaTitle: "Eleventy multilangues",
+        metaDescription: "Une demo eleventy multilangues",
+        back: "Retour",
+        skipText: "Passer au contenu principal"
+  }
+};
